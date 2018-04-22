9a610ebea553eb9049d6e7074dedfa384c0fa940
lihaixin <lihaixin@15099.net>
2018-04-22 14:10:10 +0800

    add tc

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,24 @@
 FROM alpine:latest
 
-LABEL maintainer "Darian Raymond <admin@v2ray.com>"
+LABEL maintainer "lEE <admin@v2ray.com>"
+LABEL VERSION v3.18.1
+LABEL CTIME 2018-4-22
+
+ENV PORT 59028
+ENV ID 88888888-4444-4444-4444-121212121212 
+ENV LIMIT_CONN 50
+ENV RATE 20mbit
+ENV EXECFILE	/usr/sbin/httpv
+
+RUN apk add -U iproute2 tzdata && ln -s /usr/lib/tc /lib/tc \
+         && cp -r -f /usr/share/zoneinfo/Hongkong /etc/localtime
 
 ADD https://storage.googleapis.com/v2ray-docker/v2ray /usr/bin/v2ray/
 ADD https://storage.googleapis.com/v2ray-docker/v2ctl /usr/bin/v2ray/
 ADD https://storage.googleapis.com/v2ray-docker/geoip.dat /usr/bin/v2ray/
 ADD https://storage.googleapis.com/v2ray-docker/geosite.dat /usr/bin/v2ray/
 
-COPY config.json /etc/v2ray/config.json
+# COPY config.json /etc/v2ray/config.json
 
 RUN set -ex && \
     apk --no-cache add ca-certificates && \
@@ -15,6 +26,9 @@ RUN set -ex && \
     chmod +x /usr/bin/v2ray/v2ctl && \
     chmod +x /usr/bin/v2ray/v2ray
 
-ENV PATH /usr/bin/v2ray:$PATH
+# ENV PATH /usr/bin/v2ray:$PATH
 
-CMD ["v2ray", "-config=/etc/v2ray/config.json"]
+COPY entrypoint.sh /entrypoint.sh
+RUN chmod +x /entrypoint.sh
+
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,89 @@
+#!/bin/sh
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+if [ ! -f "$EXECFILE" ]; then
+	ln -s  /usr/bin/v2ray/v2ray $EXECFILE
+fi
+sleep 2
+
+cat >> /etc/config.json<< TEMPEOF
+{
+    "log": {
+        "access": "/dev/stdout",
+        "error": "/dev/stdout",
+        "loglevel": "warning"
+    },
+    "inbound": {
+        "port": $PORT,
+        "protocol": "vmess",
+        "settings": {
+            "udp": true,
+            "clients": [
+                {
+                    "id": "$ID",
+                    "level": 1,
+                    "alterId": 233
+                }
+            ]
+        }
+    },
+    "outbound": {
+        "protocol": "freedom",
+        "settings": {}
+    },
+    "outboundDetour": [
+        {
+            "protocol": "blackhole",
+            "settings": {},
+            "tag": "blocked"
+        }
+    ],
+    "routing": {
+        "strategy": "rules",
+        "settings": {
+            "rules": [
+                {
+                    "type": "field",
+                    "ip": [
+                        "0.0.0.0/8",
+                        "10.0.0.0/8",
+                        "100.64.0.0/10",
+                        "127.0.0.0/8",
+                        "169.254.0.0/16",
+                        "172.16.0.0/12",
+                        "192.0.0.0/24",
+                        "192.0.2.0/24",
+                        "192.168.0.0/16",
+                        "198.18.0.0/15",
+                        "198.51.100.0/24",
+                        "203.0.113.0/24",
+                        "::1/128",
+                        "fc00::/7",
+                        "fe80::/10"
+                    ],
+                    "outboundTag": "blocked"
+                }
+            ]
+        }
+    }
+}
+
+TEMPEOF
+
+sleep 2
+
+
+
+LIMIT_PORT=$PORT
+BURST=100kb
+LATENCY=50ms
+INTERVAL=60
+
+iptables -F
+iptables -A INPUT -p tcp -m state --state NEW --dport $LIMIT_PORT -m connlimit --connlimit-above $LIMIT_CONN -j DROP
+tc qdisc add dev eth0 root tbf rate $RATE burst $BURST latency $LATENCY
+# watch -n $INTERVAL tc -s qdisc ls dev eth0
+
+$EXECFILE -config=/etc/config.json
+
