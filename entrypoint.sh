99071f905519db1537956c5c377f2572250fe176
lihaixin <lihaixin@15099.net>
2018-11-04 21:45:26 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,5 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 cd /usr/share/nginx/html/
-echo $V2RAYURL | base64 | tee index.html
+echo $V2RAYURL | tr ' ' '\n' | base64 | tee index.html
 nginx -g "daemon off;"
