383d61ead7684665862a4a918e9470cc36a17c34
lihaixin <lihaixin@15099.net>
2019-03-29 11:44:39 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,111 +9,68 @@ sleep 2
 
 cat > /etc/config.json<< TEMPEOF
 {
-  "log": {
-    "access": "",
-    "error": "",
-    "loglevel": ""
-  },
-  "inbound": {
-    "port": $LPORT,
-    "listen": "0.0.0.0",
-    "protocol": "socks",
-    "settings": {
-      "auth": "noauth",
-      "udp": true,
-      "ip": "127.0.0.1",
-      "clients": null
+    "log": {
+        "access": "/dev/stdout",
+        "error": "/dev/stdout",
+        "loglevel": "warning"
     },
-    "streamSettings": null
-  },
-  "outbound": {
-    "tag": "agentout",
-    "protocol": "vmess",
-    "settings": {
-      "vnext": [
+    "outbounds": [
         {
-          "address": "$SERVERIP",
-          "port": $PORT,
-          "users": [
-            {
-              "id": "$ID",
-              "alterId": 0,
-              "security": "aes-128-gcm"
+            "protocol": "vmess",
+            "settings": {
+                "vnext": [
+                    {
+                        "address": "$SERVERIP",
+                        "port": $PORT,
+                        "users": [
+                            {
+                                "id": "$ID",
+                                "alterId": 233,
+                                "security": "auto"
+                            }
+                        ]
+                    }
+                ]
+            },
+            "mux": {
+                "enabled": true
             }
-          ]
-        }
-      ],
-      "servers": null
-    },
-    "streamSettings": {
-      "network": "tcp",
-      "security": "",
-      "tlsSettings": null,
-      "tcpSettings": null,
-      "kcpSettings": null,
-      "wsSettings": null,
-      "httpSettings": null
-    },
-    "mux": {
-      "enabled": true
-    }
-  },
-  "inboundDetour": null,
-  "outboundDetour": [
-    {
-      "protocol": "freedom",
-      "settings": {
-        "response": null
-      },
-      "tag": "direct"
-    },
-    {
-      "protocol": "blackhole",
-      "settings": {
-        "response": {
-          "type": "http"
-        }
-      },
-      "tag": "blockout"
-    }
-  ],
-  "dns": {
-    "servers": [
-      "8.8.8.8",
-      "8.8.4.4",
-      "localhost"
-    ]
-  },
-  "routing": {
-    "strategy": "rules",
-    "settings": {
-      "domainStrategy": "IPIfNonMatch",
-      "rules": [
+        },
         {
-          "type": "field",
-          "port": null,
-          "outboundTag": "direct",
-          "ip": [
-            "0.0.0.0/8",
-            "10.0.0.0/8",
-            "100.64.0.0/10",
-            "127.0.0.0/8",
-            "169.254.0.0/16",
-            "192.0.0.0/24",
-            "192.0.2.0/24",
-            "192.168.0.0/16",
-            "198.18.0.0/15",
-            "198.51.100.0/24",
-            "203.0.113.0/24",
-            "::1/128",
-           "fc00::/7",
-           "fe80::/10"
-          ],
-          "domain": null
+            "protocol": "freedom",
+            "settings": {},
+            "tag": "direct"
         }
-      ]
+    ],
+    "inbounds": [
+        {
+            "port": $LSPORT,
+            "listen": "0.0.0.0",
+            "protocol": "socks",
+            "settings": {
+                "auth": "noauth",
+                "udp": true,
+                "ip": "127.0.0.1"
+            }
+        },
+        {
+            "port": $LHPORT,
+            "listen": "0.0.0.0",
+            "protocol": "http",
+            "settings": {
+                "auth": "noauth",
+                "udp": true,
+                "ip": "127.0.0.1"
+            }
+        }
+    ],
+    "dns": {
+        "servers": [
+            "8.8.8.8",
+            "8.8.4.4",
+            "localhost"
+        ]
     }
-  }
 }
 TEMPEOF
 
