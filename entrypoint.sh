02b7fee438862ec312e5ab5981a1446f7b3ffa0f
lihaixin <lihaixin@15099.net>
2019-04-05 23:55:05 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,11 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 cd /usr/share/nginx/html/
-echo $V2RAYURL | base64 | xargs | sed 's/\s\+//g' | tee index.html
-echo $V2RAYURL > index.html
+ALLURL=`env | grep A2RAYURL`
+for c in $ALLURL
+do
+        echo $c | awk -F'vmess' -vOFS="vmess" '{$1="";$1=$1}1' >> url.txt
+done
+echo url.txt | base64 | xargs | sed 's/\s\+//g' | tee index.html
+rm -rf  url.txt
 nginx -g "daemon off;"
