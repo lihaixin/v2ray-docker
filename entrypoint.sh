e59597e3e893d275ddca95200d4f89ed8cb11001
lihaixin <lihaixin@15099.net>
2019-04-14 09:08:36 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,12 @@ export PATH
 cd /usr/share/nginx/html/
 echo "添加以V2RAYURL开头的变量增加节点"
 echo "例如V2RAYURL_US1"
+echo "详细使用方法请查看http://bit.ly/2HN3STj"
 ALLURL=`env | grep V2RAYURL`
 for c in $ALLURL
 do
         echo $c | awk -F'vmess' -vOFS="vmess" '{$1="";$1=$1}1' >> url.txt
 done
-cat url.txt | base64 | xargs | sed 's/\s\+//g' | tee index.html
+cat url.txt | base64 | xargs | sed 's/\s\+//g' | tee index.html >/dev/null
 rm -rf  url.txt
-nginx -g "daemon off;"
+nginx -g "daemon off;" >/dev/null
